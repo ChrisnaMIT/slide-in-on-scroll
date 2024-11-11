c88bae88bdc50a3ffa437e993e7eef1a758b0a80
chrisna <chrisna.mitt@gmail.com>
2024-11-11 13:45:45 +0100

    test1

---
+++ js/main.js
@@ -0,0 +1,38 @@
+function debounce(func, wait = 20, immediate = true) {
+    var timeout;
+    return function() {
+        var context = this, args = arguments;
+        var later = function() {
+            timeout = null;
+            if (!immediate) func.apply(context, args);
+        };
+        var callNow = immediate && !timeout;
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+        if (callNow) func.apply(context, args);
+    };
+}
+
+const sliderImages = document.querySelectorAll('.slide-in');
+
+function checkSlide(e) {
+    // half way through the image
+    console.log(window.scrollY);
+    sliderImages.forEach(sliderImages => {
+       const slideInAt = (window.scrollY + window.innerHeight) - sliderImages.height / 2;
+       //bottom of the image
+       console.log(slideInAt);
+       const imageBottom = sliderImages.offsetTop + sliderImages.height;
+       const isHalfShown = slideInAt > sliderImages.offsetTop;
+       const isNotScrolledPast = window.scrollY < imageBottom;
+       if (isHalfShown && isNotScrolledPast) {
+           sliderImages.classList.add('active');
+       } else {
+           sliderImages.classList.remove('active');
+       }
+
+    });
+
+}
+
+window.addEventListener('scroll', debounce(checkSlide));
